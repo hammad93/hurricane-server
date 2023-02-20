4bb74da0996954775c01f4478d8d8835ccb53d11
Hammad Usmani <ham.u@live.com>
2023-02-19 22:55:27 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-git clone https://github.com/hammad93/hurricane-deploy.git
+git clone --single-branch --branch main https://github.com/hammad93/hurricane-deploy.git
+git clone --single-branch --branch master https://github.com/apatel726/HurricaneDissertation.git
 docker-compose up
