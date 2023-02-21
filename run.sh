acce9190fbb26cf0271ccddf12e8761a73e55523
Hammad Usmani <ham.u@live.com>
2023-02-20 22:44:53 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ cp database.key docker/
 cp django_secret.key docker/
 git clone --single-branch --branch main https://github.com/hammad93/hurricane-deploy.git
 cp credentials.csv ./hurricane-deploy/docker/
-git clone --single-branch --branch master https://github.com/apatel726/HurricaneDissertation.git
+git clone --depth 1 --single-branch --branch master https://github.com/apatel726/HurricaneDissertation.git
 docker-compose build --no-cache
 docker-compose up
