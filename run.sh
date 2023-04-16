b74e4fbc26027ece71a23a2e503ef9053992b806
hammad93 <ham.u@live.com>
2023-04-16 04:30:07 +0000

    Add additional key

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 docker image prune -f
 cp database.key docker/
 cp django_secret.key docker/
+cp hurricane-live-db.key docker/
 git clone --single-branch --branch main https://github.com/hammad93/hurricane-deploy.git
 cp credentials.csv ./hurricane-deploy/docker/
 git clone --depth 1 --single-branch --branch master https://github.com/apatel726/HurricaneDissertation.git
